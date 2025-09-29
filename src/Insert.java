f9c4255cbfba03ab0b4ddc9225b06008793c023a
Somya Saxena <saxenasomya823@gmail.com>
2025-09-29 18:05:29 +0530

    project1

---
+++ src/Insert.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+class Insert {
+    public static void main(String []s){
+        Scanner sc=new Scanner(System.in);
+        System.out.print("Enter string:");
+        String s1=sc.nextLine();
+        System.out.print("Enter index number:");
+        int x=sc.nextInt();
+        sc.nextLine();
+        System.out.print("Enter string to be inserted:");
+        String s2=sc.nextLine();
+
+
+        StringBuffer sb=new StringBuffer(s1);
+        sb.insert(x,s2);
+        s1=sb.toString();
+        System.out.print(s1);
+
+    }
+}
